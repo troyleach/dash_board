efe2c3caf185fbf4c10bb02defe5f9153ca8d966
Troy Leach <troyleach29@gmail.com>
2020-10-31 08:13:52 -0500

    added home with the basic grid layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './containers/Home';
 import TaskApp from './containers/TodoApp'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         title='Command Center'
         userName='Troy Leach'
       />
+      <Home />
 
       {/* <TaskApp /> */}
       <Footer />
---
+++ src/containers/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Home.css";
+
+
+function Home(props) {
+  return (
+    <div className="home-container">
+      <div class="wrapper">
+        <div class="box a">
+          A
+        </div>
+        <div class="box b">B</div>
+        <div class="box c">C</div>
+        <div class="box d">D</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
